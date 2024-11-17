530558e8f92ba24ee8709d92a173df6071e21902
FlagrateAE <flagrateinc@gmail.com>
2024-11-17 15:05:25 +0200

    fix: broken config

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-'ADMINS = []' 
+# Telegram Bot API token. You can acquire one here: https://t.me/BotFather
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+# The Movie Database API access token. You can acquire one here: https://developers.themoviedb.org/3/getting-started/introduction.
+# Beware that this is NOT an API key, but an access token!
+TMDB_ACCESS_TOKEN = "YOUR_TMDB_ACCESS_TOKEN"
+# The path to the database file. This file will be created if it doesn't exist.
+DB_PATH = "database.db"
+# List of Telegram user IDs that are allowed to use a restricted command /all
+ADMINS = []
